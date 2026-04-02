acc8a559def18b2e195f8d46526303d8987b2144
mrtnbtstaa <martinbautista0288@outlook.com>
2026-04-02 22:30:53 +0800

    chore: add config folder to .gitignore and untrack

--- lib/core/events/auth_events.dart
+++
@@ -1,7 +0,0 @@
-import 'package:chat/core/events/i_event_bus.dart';
-import 'package:equatable/equatable.dart';
-
-class LogoutEvent extends Equatable implements IEventBus{
-  @override
-  List<Object?> get props => [];
-}
